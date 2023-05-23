dfc9c70d8d645915fc1c7df7df9d1efabd78a93a
Miron Sadykov <reagent992@gmail.com>
2023-05-23 13:33:41 +0300

    Сохранение изменений перед мерджем

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# Пример использования пермишенов только для определённых ролей
+#
+# from rest_framework.views import APIView
+# from rest_framework.permissions import IsAuthenticated
+# from .permissions import AdminOnlyPermission
+#
+# class AdminOnlyView(APIView):
+#     Пермишен для роли администратор:
+#     permission_classes = [IsAuthenticated, AdminPermission]
+#     pass
+#
 
-# Create your views here.
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -10,7 +10,7 @@ CHOICES = (
 
 class User(AbstractUser):
     """
-    Добавление полей Биографии и Роли для модели User
+    Добавление полей Биографии и Роли для модели User.
     """
     bio = models.TextField(blank=True)
     role = models.CharField(max_length=50, choices=CHOICES, default='user')
