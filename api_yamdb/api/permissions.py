493f66975d5090cdb06e2179830b13d72e26f2c1
Miron Sadykov <reagent992@gmail.com>
2023-06-01 16:18:38 +0300

    permissions.py fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class AdminOnlyPermission(BasePermission):
@@ -66,14 +66,13 @@ class ReviewsAndCommentsPermission(BasePermission):
     """
 
     def has_permission(self, request, view):
-        if view.action in ['list', 'retrieve']:
+        if request.method in SAFE_METHODS:
             return True
-        if view.action in ['create'] and request.user.is_authenticated:
+        if request.user.is_authenticated:
             return True
-        obj = view.get_object()
         if hasattr(request.user, 'role'):
             return (request.user.role in ['admin', 'moderator']
-                    or request.user.is_superuser or obj.author == request.user)
+                    or request.user.is_superuser)
 
     def has_object_permission(self, request, view, obj):
         if view.action in ['retrieve']:
